2a03dec32a72bd296acc15050fe04d7bfff5140c
Tine <tine@equaleyes.com>
2017-09-25 23:36:54 +0100

    Test: add test for video update

--- src/apps/video/tests/test_video.py
+++ src/apps/video/tests/test_video.py
@@ -14,7 +14,7 @@ class FlaskrTestCase(VideoTestBase):
 
     def test_database_entry(self):
 
-        response = self.app.post('/video-database',
+        response = self.app.post('/video-to-database',
                                  content_type='application/json',
                                  data=json.dumps(dict(
                                      title='CNN Video',
@@ -25,4 +25,22 @@ class FlaskrTestCase(VideoTestBase):
 
         video = Video.query.filter_by(title='CNN Video').first()
         self.assertEqual(video.title, 'CNN Video')
+        self.assertEqual(video.description, 'Video description')
         self.assertEqual(response.status_code, 200)
+
+    def test_video_update(self):
+        response = self.app.post('/video-update',
+                                 content_type='application/json',
+                                 data=json.dumps(dict(
+                                     id='1',
+                                     title='New CNN Video',
+                                     description='New description')))
+
+        video = Video.query.filter_by(id='1').first()
+        self.assertEqual(video.title, 'New CNN Video')
+        self.assertEqual(video.description, 'New description')
+        self.assertEqual(response.status_code, 200)
+
+    # def test_get_all_videos(self):
+
+    # def test_get_video_query(self):
